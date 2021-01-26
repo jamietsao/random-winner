e66fa4b67a855899007cfba50d6d1335cda1688d
Jamie Tsao <jamie@gametime.co>
2021-01-25 22:42:45 -0800

    Add support to supply names via command line flag

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
+	"os"
 	"strings"
 	"time"
 )
@@ -11,51 +13,57 @@ const (
 	target = 1000
 )
 
-// backend team
-var team = []string{
-	"Brandon",
-	"Bigo",
-	"Jeff",
-	"Benny",
-	"Andy",
-	"Eric",
-	"Leilani",
-	"Carmen",
+func init() {
+	// seed rng
+	rand.Seed(time.Now().UnixNano())
 }
 
 func main() {
-	// seed it!
-	rand.Seed(time.Now().UnixNano())
+	var nameStr string
+
+	// parse flags
+	flag.StringVar(&nameStr, "names", "", "Comma separated list of names")
+	flag.Parse()
+
+	names := strings.Split(nameStr, ",")
+
+	if len(names) < 2 {
+		fmt.Println("Must supply at least two names via -names flag:")
+		flag.Usage()
+		os.Exit(0)
+	}
 
 	// pick winner
-	winner()
+	winner(names)
 }
 
-func winner() {
-	fmt.Printf("First to %d wins!", target)
-	fmt.Printf("%s", strings.Repeat("\n", len(team)))
+func winner(names []string) {
+	count := len(names)
+
+	fmt.Printf("First to %d wins!\n", target)
+	fmt.Printf("%s", strings.Repeat("\n", count))
 
 	// run until winner is determined
 	winner := ""
 	results := make(map[string]int)
 	for {
 		// random choice
-		i := rand.Intn(len(team))
-		results[team[i]]++
+		i := rand.Intn(count)
+		results[names[i]]++
 
 		// move cursor back to top left
-		fmt.Printf("\u001b[%dD\u001b[%dA", 50, len(team))
+		fmt.Printf("\u001b[%dD\u001b[%dA", 50, count)
 
 		// print current progress
 		multiplier := target / 100
-		for _, name := range team {
+		for _, name := range names {
 			width := results[name] / (2 * multiplier)
 			fmt.Printf("[%10v => %s%s]\n", name, strings.Repeat("#", width), strings.Repeat(" ", 50-width))
 		}
 
 		// stop once someone hits the target
-		if results[team[i]] == target {
-			winner = team[i]
+		if results[names[i]] == target {
+			winner = names[i]
 			break
 		}
 
@@ -64,7 +72,7 @@ func winner() {
 
 	// display final results
 	fmt.Printf("\nFinal tally:\n")
-	for _, name := range team {
+	for _, name := range names {
 		fmt.Printf("%10v: %d\n", name, results[name])
 	}
 
